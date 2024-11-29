397bb24c91583d4af770b70c0a18dd891cf934c6
viaanneyyovo <vianney.yovo@epitech.eu>
2024-11-29 12:33:12 +0000

    [UPDATE]: facebook cfrome extension folden

--- backend/src/linkedin/dto/update-linkedin.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLinkedinDto } from './create-linkedin.dto';
-
-export class UpdateLinkedinDto extends PartialType(CreateLinkedinDto) {}
--- backend/src/linkedin/linkedin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LinkedInService } from './linkedin.service';
-import { LinkedInController } from './linkedin.controller';
-
-@Module({
-  controllers: [LinkedInController],
-  providers: [LinkedInService],
-})
-export class LinkedInModule {}
