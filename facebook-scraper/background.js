b94cd5f72a42d339d5d72f23c748102c5a284737
viaanneyyovo <vianney.yovo@epitech.eu>
2024-12-24 13:28:38 +0000

    [INIT]: initialization

--- facebook-scraper/background.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Facebook Scraper Extension Installed");
-});
--- instagram_ashtag_chrome_extension/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("scrapeBtn").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        files: ["content.js"],
-      });
-    });
-  });
