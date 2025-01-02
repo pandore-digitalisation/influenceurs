c92c403ebd13a918c0cc9094a85413dc0461a0ec
viaanneyyovo <vianney.yovo@epitech.eu>
2025-01-02 15:50:26 +0000

    [UPDATE]: login

--- influencer_frontend/app/login/page.tsx
+++ influencer_frontend/app/login/page.tsx
@@ -11,7 +11,7 @@ export default function Login() {
       <div className="flex flex-col items-center">
         <div>logo</div>
         <div className="flex flex-col items-center my-52">
-          <span className="my-6 text-black text-2xl">Welcome back</span>
+          <span className="my-6 text-black text-2xl">Welcomes back</span>
           <button
             type="button" onClick={() => router.push('/dashboard') }
             className="flex py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
