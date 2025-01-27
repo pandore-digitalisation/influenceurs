75522726c0aebc6bc3b0261aac73f973cb4406ba
viaanneyyovo <vianney.yovo@epitech.eu>
2025-01-27 22:00:15 +0000

    [FIX]: redirection

--- influencer-backend/src/main.ts
+++ influencer-backend/src/main.ts
@@ -6,9 +6,18 @@ async function bootstrap() {
 
   // Activer CORS pour des origines spécifiques (exemple : Instagram et une extension Chrome)
   app.enableCors({
-    origin: '*', // Liste des origines autorisées
-    methods: ['GET', 'POST', 'DELETE', 'PATCH'], // Méthodes HTTP autorisées
-    allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
+    origin: [
+      'http://localhost:3001',
+      'https://pandoreinfluencerfrontend.vercel.app',
+      'https://www.instagram.com',
+      'https://www.facebook.com',
+      'https://www.linkedin.com',
+      'https://x.com',
+      'https://www.tiktok.com',
+      'chrome-extension://dnkjkcnopgdjhmkhapenjopjpiikaljc',
+    ], // Liste des origines autorisées
+    methods: ['GET,POST, DELETE, PATCH'], // Méthodes HTTP autorisées
+    allowedHeaders: ['Content-Type, Authorization'], // En-têtes autorisés
     credentials: true,
   });
 
@@ -19,34 +28,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   // Activer CORS pour des origines spécifiques (exemple : Instagram et une extension Chrome)
-//   app.enableCors({
-//     origin: [
-//       'http://localhost:3001',
-//       'https://pandoreinfluencerfrontend.vercel.app',
-//       'https://www.instagram.com',
-//       'https://www.facebook.com',
-//       'https://www.linkedin.com',
-//       'https://x.com',
-//       'https://www.tiktok.com',
-//       'chrome-extension://dnkjkcnopgdjhmkhapenjopjpiikaljc',
-//     ], // Liste des origines autorisées
-//     methods: ['GET', 'POST', 'DELETE', 'PATCH'], // Méthodes HTTP autorisées
-//     allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
-//     credentials: true,
-//   });
-
-//   const port = process.env.PORT ?? 3000;
-//   console.log(`Application is running on: http://localhost:${port}`);
-
-//   await app.listen(port);
-// }
-
-// bootstrap();
