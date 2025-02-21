2ab5149b4786da17029eb8cc741166ac34001075
viaanneyyovo <vianney.yovo@epitech.eu>
2025-02-21 16:18:33 +0000

    [UPDATE]: facebook conte.js

--- aquizition_extension/scripts/content_facebook.js
+++ aquizition_extension/scripts/content_facebook.js
@@ -1,185 +1,281 @@
 (async () => {
-  const BASE_URL = "https://influenceur-list.onrender.com";
-  // const BASE_URL = "http://localhost:3000";
+  const BASE_URL = "http://localhost:3000";
 
-  function getXPathText(xpath, attr = "textContent") {
-    const node = document.evaluate(
-      xpath,
-      document,
-      null,
-      XPathResult.FIRST_ORDERED_NODE_TYPE,
-      null
-    ).singleNodeValue;
-    return node
-      ? attr === "textContent"
-        ? node.textContent.trim()
-        : node[attr]
-      : null;
-  }
-
-  function cleanNumber(value) {
-    if (!value) return "None";
-  
-    // Retirer tous les caractères non numériques, sauf 'K' et 'M'
-    let cleanedValue = value.replace(/[^\d.,KM]/g, "");
-
-    cleanedValue = cleanedValue.replace(",", ".");
-  
-    // Si la valeur se termine par "M"
-    if (cleanedValue.endsWith("M")) {
-      return Math.round(parseFloat(cleanedValue.replace("M", "")) * 1000000);
-    }
-  
-    // Si la valeur se termine par "K"
-    if (cleanedValue.endsWith("K")) {
-      return Math.round(parseFloat(cleanedValue.replace("K", "")) * 1000);
-    }
-  
-    // Si la valeur n'a ni "M" ni "K", retour du nombre tel quel
-    return Math.round(parseFloat(cleanedValue));
-  }
-  
-  // function cleanNumber(value) {
-  //   if (!value) return "0";
-  //   let cleanedValue = value.replace(/[^\dKM.]/g, "");
-
-  //   if (cleanedValue.endsWith("M"))
-  //     return parseFloat(
-  //       cleanedValue.replace("M", "").replace(",", "") * 1000000
-  //     );
-  //   if (cleanedValue.endsWith("K"))
-  //     return parseFloat(cleanedValue.replace("K", "").replace(",", "") * 1000);
-  //   return cleanedValue;
-  // }
-
-  const xPaths = {
-    name: "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[3]/div/div/div[1]/div/div/span/h1/text()[1]",
-    followers:
-      "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[3]/div/div/div[2]/span/a[1]/text()[1]",
-    following:
-      "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[3]/div/div/div[2]/span/a[2]/text()[1]",
-    profileImage:
-      "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[1]/div/a/div",
+  const getXPathText = (xpath, attr = "textContent") => {
+    const node = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    return attr === "textContent" ? node?.textContent.trim() : node?.[attr] || null;
   };
 
-  let followers = getXPathText(xPaths.followers);
-  let following = getXPathText(xPaths.following);
+  const cleanNumber = (value) => {
+    if (!value) return 0;
+    const cleanedValue = value.replace(/[^\d.,KM]/g, "").replace(",", ".");
+    return cleanedValue.endsWith("M")
+      ? Math.round(parseFloat(cleanedValue) * 1_000_000)
+      : cleanedValue.endsWith("K")
+      ? Math.round(parseFloat(cleanedValue) * 1_000)
+      : Math.round(parseFloat(cleanedValue));
+  };
+
+  const xPaths = {
+    name: "//span/h1/text()[1]",
+    followers: "//span/a[1]/text()[1]",
+    following: "//span/a[2]/text()[1]",
+    profileImage: "//div/a/div"
+  };
 
   const extractedData = {
     name: getXPathText(xPaths.name) || "None",
-    followers: cleanNumber(followers) || "0",
-    following: cleanNumber(following) || "0",
+    followers: cleanNumber(getXPathText(xPaths.followers)),
+    following: cleanNumber(getXPathText(xPaths.following)) || "0",
     profileImage: getXPathText(xPaths.profileImage, "src") || " ",
     profileUrl: window.location.href,
-    plateform: "Facebook",
+    plateform: "Facebook"
   };
 
   console.log("Extracted Data:", extractedData);
 
-  const getUserData = () =>
-    new Promise((resolve, reject) => {
-      chrome.storage.sync.get("userData", (result) =>
-        chrome.runtime.lastError
-          ? reject(new Error(chrome.runtime.lastError))
-          : resolve(result.userData)
-      );
-    });
+  const getStorageData = (key) =>
+    new Promise((resolve, reject) =>
+      chrome.storage.sync.get(key, (result) => chrome.runtime.lastError ? reject(new Error(chrome.runtime.lastError)) : resolve(result[key]))
+    );
 
-  const getExistingProfile = async (profileUrl) => {
-    try {
-      const response = await fetch(
-        `${BASE_URL}/facebook/${encodeURIComponent(profileUrl)}`
-      );
-      return response.ok ? response.json() : null;
-    } catch (error) {
-      console.error("Error fetching profile:", error);
-      return null;
+  try {
+    const [userData, selectedList] = await Promise.all([getStorageData("userData"), getStorageData("selectedList")]);
+
+    if (!userData?.data?.userId) {
+      console.error("User not logged in.");
+      return;
     }
-  };
 
-  let userData;
-  try {
-    userData = await getUserData();
-  } catch (error) {
-    console.error(error);
-    return;
-  }
+    const currentUserId = userData.data.userId;
 
-  // Fonction pour récupérer selectedList de manière asynchrone
-  const getSelectedList = () => {
-    return new Promise((resolve, reject) => {
-      chrome.storage.sync.get(["selectedList"], function (result) {
-        if (chrome.runtime.lastError) {
-          reject(new Error(chrome.runtime.lastError));
-        } else {
-          resolve(result.selectedList);
-        }
+    const existingProfile = await fetch(`${BASE_URL}/facebook/${encodeURIComponent(extractedData.profileUrl)}`)
+      .then((res) => res.ok ? res.json() : null)
+      .catch((error) => {
+        console.error("Error fetching profile:", error);
+        return null;
       });
-    });
-  };
 
-  let selectedList;
+    extractedData.userId = existingProfile?.userId?.includes(currentUserId)
+      ? existingProfile.userId
+      : [...(existingProfile?.userId || []), currentUserId];
 
-  try {
-    selectedList = await getSelectedList(); // Attends que selectedList soit récupéré
-    console.log("selectedList récupéré:", selectedList);
-  } catch (error) {
-    console.error("Erreur lors de la récupération de selectedList:", error);
-  }
-
-  if (!userData?.data?.userId) {
-    console.error("User not logged in or missing data.");
-    return;
-  }
-
-  const currentUserId = userData.data.userId;
-  const existingProfile = await getExistingProfile(extractedData.profileUrl);
-
-  extractedData.userId = existingProfile?.userId?.includes(currentUserId)
-    ? existingProfile.userId
-    : [...(existingProfile?.userId || []), currentUserId];
-
-  const isValidData = ({ name, followers, following }) =>
-    name !== "None" && followers !== "None" && following !== "None";
-
-  if (isValidData(extractedData)) {
-    try {
+    if (extractedData.name !== "None" && extractedData.followers && extractedData.following) {
       const response = await fetch(`${BASE_URL}/facebook`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(extractedData),
+        body: JSON.stringify(extractedData)
       });
 
       const data = await response.json();
-
-      console.log("data to send to list", data);
+      console.log("Data sent:", data);
 
       if (selectedList) {
-        const profilesUpdate = {
-          profiles: {
-            add: [data],
-          },
-        };
-
         await fetch(`${BASE_URL}/lists/${selectedList}`, {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(profilesUpdate),
+          body: JSON.stringify({ profiles: { add: [data] } })
         });
-        console.log("List update");
+        console.log("List updated");
       } else {
-        console.log("List not update");
+        console.log("No list update");
       }
 
-      const success = response.ok;
-      console.log("Success:", success);
-      chrome.runtime.sendMessage({ success });
-    } catch (error) {
-      console.error("Network error:", error);
-      chrome.runtime.sendMessage({ networkError });
+      chrome.runtime.sendMessage({ success: response.ok });
+    } else {
+      console.warn("Invalid data. Skipping POST request.");
+      chrome.runtime.sendMessage({ dataNotExtracted: true });
     }
-  } else {
-    console.warn("Invalid data. Skipping POST request.");
-    chrome.runtime.sendMessage({ dataNotExtracted: true });
+  } catch (error) {
+    console.error("Error:", error);
+    chrome.runtime.sendMessage({ error: true });
   }
 })();
+
+
+
+// (async () => {
+//   // const BASE_URL = "https://influenceur-list.onrender.com";
+//   const BASE_URL = "http://localhost:3000";
+
+//   function getXPathText(xpath, attr = "textContent") {
+//     const node = document.evaluate(
+//       xpath,
+//       document,
+//       null,
+//       XPathResult.FIRST_ORDERED_NODE_TYPE,
+//       null
+//     ).singleNodeValue;
+//     return node
+//       ? attr === "textContent"
+//         ? node.textContent.trim()
+//         : node[attr]
+//       : null;
+//   }
+
+//   function cleanNumber(value) {
+//     if (!value) return "None";
+  
+//     // Retirer tous les caractères non numériques, sauf 'K' et 'M'
+//     let cleanedValue = value.replace(/[^\d.,KM]/g, "");
+
+//     cleanedValue = cleanedValue.replace(",", ".");
+  
+//     // Si la valeur se termine par "M"
+//     if (cleanedValue.endsWith("M")) {
+//       return Math.round(parseFloat(cleanedValue.replace("M", "")) * 1000000);
+//     }
+  
+//     // Si la valeur se termine par "K"
+//     if (cleanedValue.endsWith("K")) {
+//       return Math.round(parseFloat(cleanedValue.replace("K", "")) * 1000);
+//     }
+  
+//     // Si la valeur n'a ni "M" ni "K", retour du nombre tel quel
+//     return Math.round(parseFloat(cleanedValue));
+//   }
+  
+//   // function cleanNumber(value) {
+//   //   if (!value) return "0";
+//   //   let cleanedValue = value.replace(/[^\dKM.]/g, "");
+
+//   //   if (cleanedValue.endsWith("M"))
+//   //     return parseFloat(
+//   //       cleanedValue.replace("M", "").replace(",", "") * 1000000
+//   //     );
+//   //   if (cleanedValue.endsWith("K"))
+//   //     return parseFloat(cleanedValue.replace("K", "").replace(",", "") * 1000);
+//   //   return cleanedValue;
+//   // }
+
+//   const xPaths = {
+//     name: "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[3]/div/div/div[1]/div/div/span/h1/text()[1]",
+//     followers:
+//       "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[3]/div/div/div[2]/span/a[1]/text()[1]",
+//     following:
+//       "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[3]/div/div/div[2]/span/a[2]/text()[1]",
+//     profileImage:
+//       "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div/div/div[1]/div/a/div",
+//   };
+
+//   let followers = getXPathText(xPaths.followers);
+//   let following = getXPathText(xPaths.following);
+
+//   const extractedData = {
+//     name: getXPathText(xPaths.name) || "None",
+//     followers: cleanNumber(followers) || "0",
+//     following: cleanNumber(following) || "0",
+//     profileImage: getXPathText(xPaths.profileImage, "src") || " ",
+//     profileUrl: window.location.href,
+//     plateform: "Facebook",
+//   };
+
+//   console.log("Extracted Data:", extractedData);
+
+//   const getUserData = () =>
+//     new Promise((resolve, reject) => {
+//       chrome.storage.sync.get("userData", (result) =>
+//         chrome.runtime.lastError
+//           ? reject(new Error(chrome.runtime.lastError))
+//           : resolve(result.userData)
+//       );
+//     });
+
+//   const getExistingProfile = async (profileUrl) => {
+//     try {
+//       const response = await fetch(
+//         `${BASE_URL}/facebook/${encodeURIComponent(profileUrl)}`
+//       );
+//       return response.ok ? response.json() : null;
+//     } catch (error) {
+//       console.error("Error fetching profile:", error);
+//       return null;
+//     }
+//   };
+
+//   let userData;
+//   try {
+//     userData = await getUserData();
+//   } catch (error) {
+//     console.error(error);
+//     return;
+//   }
+
+//   // Fonction pour récupérer selectedList de manière asynchrone
+//   const getSelectedList = () => {
+//     return new Promise((resolve, reject) => {
+//       chrome.storage.sync.get(["selectedList"], function (result) {
+//         if (chrome.runtime.lastError) {
+//           reject(new Error(chrome.runtime.lastError));
+//         } else {
+//           resolve(result.selectedList);
+//         }
+//       });
+//     });
+//   };
+
+//   let selectedList;
+
+//   try {
+//     selectedList = await getSelectedList(); // Attends que selectedList soit récupéré
+//     console.log("selectedList récupéré:", selectedList);
+//   } catch (error) {
+//     console.error("Erreur lors de la récupération de selectedList:", error);
+//   }
+
+//   if (!userData?.data?.userId) {
+//     console.error("User not logged in or missing data.");
+//     return;
+//   }
+
+//   const currentUserId = userData.data.userId;
+//   const existingProfile = await getExistingProfile(extractedData.profileUrl);
+
+//   extractedData.userId = existingProfile?.userId?.includes(currentUserId)
+//     ? existingProfile.userId
+//     : [...(existingProfile?.userId || []), currentUserId];
+
+//   const isValidData = ({ name, followers, following }) =>
+//     name !== "None" && followers !== "None" && following !== "None";
+
+//   if (isValidData(extractedData)) {
+//     try {
+//       const response = await fetch(`${BASE_URL}/facebook`, {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(extractedData),
+//       });
+
+//       const data = await response.json();
+
+//       console.log("data to send to list", data);
+
+//       if (selectedList) {
+//         const profilesUpdate = {
+//           profiles: {
+//             add: [data],
+//           },
+//         };
+
+//         await fetch(`${BASE_URL}/lists/${selectedList}`, {
+//           method: "PUT",
+//           headers: { "Content-Type": "application/json" },
+//           body: JSON.stringify(profilesUpdate),
+//         });
+//         console.log("List update");
+//       } else {
+//         console.log("List not update");
+//       }
+
+//       const success = response.ok;
+//       console.log("Success:", success);
+//       chrome.runtime.sendMessage({ success });
+//     } catch (error) {
+//       console.error("Network error:", error);
+//       chrome.runtime.sendMessage({ networkError });
+//     }
+//   } else {
+//     console.warn("Invalid data. Skipping POST request.");
+//     chrome.runtime.sendMessage({ dataNotExtracted: true });
+//   }
+// })();
